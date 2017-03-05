27a6a86efcf443e0bd8b85236c49131e140116e0
gfyoung <gfyoung17@gmail.com>
2017-03-05 01:25:34 -0500

    Refactor game simulator code

--- gameSim.py
+++ gameSim.py
@@ -1,8 +1,9 @@
 """
-Next Make CPW 2017 Game Simulator
+Example game that you can use as a template for your own games!
 
-This is where you will also write the code
-to develop the game you so desire! :)
+This code contains a lot of useful functions that you can use
+for coming up with ideas for your own games, such as how you
+move characters around.
 """
 
 import sys
@@ -10,85 +11,141 @@ import time
 import pygame
 from pygame.locals import *
 
-# EDIT THIS SECTION OF CODE AT YOUR OWN RISK.
-pygame.init()
-screen = pygame.display.set_mode((550, 400))
-pygame.display.set_caption('Pygame Keyboard Test')
-pygame.mouse.set_visible(0)
+# Window dimensions
+WINDOWWIDTH = 550
+WINDOWHEIGHT = 400
 
+# Game board colors
 WHITE = 255, 255, 255
 GREEN = 0, 255, 0
-BLACK = 0, 0, 0
 BLUE = 0, 0, 255
+BLACK = 0, 0, 0
 RED = 255, 0, 0
 
-# Array for keeping tracked of relevant
-# game keys and whether they have been
-# pressed or not.
-#
-# 0 = Key Not Pressed
-# 1 = Key Pressed
-KEYS = [0, 0, 0, 0, 0, 0, 0, 0]
-
+# Key syntactic sugars
 KEY_W = 0
 KEY_A = 1
 KEY_S = 2
 KEY_D = 3
+
 KEY_UP = 4
 KEY_DOWN = 5
 KEY_LEFT = 6
 KEY_RIGHT = 7
 
-# The screen is a 7x5 array and is the only item
-# that should be modified during your game.
-SCREEN_BUFFER = [[0, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 0]]
+# Game global variables
+KEYS = None
+CHAR_X = None
+CHAR_Y = None
+SURFACE = None
+TIMEOUT = None
+DISP_COUNTER = None
+SCREEN_BUFFER = None
+
+
+def main():
+    global CHAR_X, CHAR_Y, DISP_COUNTER, KEYS, SCREEN_BUFFER, SURFACE, TIMEOUT
+
+    pygame.init()
+
+    title = "Pygame Keyboard Test"
+    SURFACE = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+
+    pygame.display.set_caption(title)
+    pygame.mouse.set_visible(False)
+
+    # The display counter used for setting how often
+    # we update the screen buffer.
+    DISP_COUNTER = 0
+
+    # The screen is a 7x5 array and is the only item
+    # that should be modified during your game.
+    SCREEN_BUFFER = [[0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0]]
+
+    # Location for a character pixel
+    CHAR_X = 0
+    CHAR_Y = 0
+
+    # Counter used when moving a character
+    TIMEOUT = 0
+
+    # Array for keeping tracked of relevant
+    # game keys and whether they have been
+    # pressed or not.
+    #
+    # 0 = Key Not Pressed
+    # 1 = Key Pressed
+    KEYS = [0, 0, 0, 0, 0, 0, 0, 0]
+
+    while True:
+        key_test()
+        # move_character()
+        # your_program()
+
+        # Needed for simulation to run.
+        update_simulator()
 
 
 def clear_buffer():
     """
     Clear the screen buffer.
     """
-    global SCREEN_BUFFER
 
     for x in range(7):
         for y in range(5):
             SCREEN_BUFFER[x][y] = 0
 
 
-DISP_COUNTER = 0
+def draw_screen():
+    """
+    Redraw the screen using the updated screen buffer.
+    """
+
+    for x in range(7):
+        for y in range(5):
+            color = RED
+            radius = 30
+            pos = 50 + 75*x, 350 - 75*y
+
+            if SCREEN_BUFFER[x][y] == 0:  # unfilled
+                pygame.draw.circle(SURFACE, color, pos, radius, 3)
+            else:  # filled
+                pygame.draw.circle(SURFACE, color, pos, radius, 0)
+
+
+def update_keys(new_keys):
+    """
+    Update the key mappings based on the new keys provided.
+
+    :param new_keys: The mappings provided from pygame for keys pressed.
+    """
+
+    KEYS[KEY_UP] = new_keys[pygame.K_UP]
+    KEYS[KEY_DOWN] = new_keys[pygame.K_DOWN]
+    KEYS[KEY_RIGHT] = new_keys[pygame.K_RIGHT]
+    KEYS[KEY_LEFT] = new_keys[pygame.K_LEFT]
+
+    KEYS[KEY_W] = new_keys[pygame.K_w]
+    KEYS[KEY_S] = new_keys[pygame.K_s]
+    KEYS[KEY_D] = new_keys[pygame.K_d]
+    KEYS[KEY_A] = new_keys[pygame.K_a]
 
 
 def update_simulator():
     """
     Update the simulator, including the display and keys pressed.
     """
+
     global DISP_COUNTER
 
-    def draw_screen():
-        """
-        Redraw the screen using the updated screen buffer.
-        """
-        global SCREEN_BUFFER
-
-        for x in range(7):
-            for y in range(5):
-                color = RED
-                radius = 30
-                pos = 50 + 75*x, 350 - 75*y
-
-                if SCREEN_BUFFER[x][y] == 0:  # unfilled --> filled
-                    pygame.draw.circle(screen, color, pos, radius, 3)
-                else:  # filled --> unfilled
-                    pygame.draw.circle(screen, color, pos, radius, 0)
-
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == QUIT:
             sys.exit()
         elif event.type == KEYDOWN and event.key == K_ESCAPE:
             pygame.display.quit()
@@ -97,15 +154,7 @@ def update_simulator():
     # Update key presses by checking
     # for pressed keys.
     keys = pygame.key.get_pressed()
-    KEYS[KEY_UP] = keys[pygame.K_UP]
-    KEYS[KEY_DOWN] = keys[pygame.K_DOWN]
-    KEYS[KEY_RIGHT] = keys[pygame.K_RIGHT]
-    KEYS[KEY_LEFT] = keys[pygame.K_LEFT]
-
-    KEYS[KEY_W] = keys[pygame.K_w]
-    KEYS[KEY_S] = keys[pygame.K_s]
-    KEYS[KEY_D] = keys[pygame.K_d]
-    KEYS[KEY_A] = keys[pygame.K_a]
+    update_keys(keys)
 
     if DISP_COUNTER >= 20:
         draw_screen()
@@ -114,7 +163,7 @@ def update_simulator():
     else:
         DISP_COUNTER += 1
 
-    screen.fill(BLACK)
+    SURFACE.fill(BLACK)
     time.sleep(0.001)
 
 
@@ -123,7 +172,6 @@ def key_test():
     """
     Test program to make sure that we're responding to all relevant keys.
     """
-    global SCREEN_BUFFER
 
     clear_buffer()
 
@@ -138,48 +186,43 @@ def key_test():
     SCREEN_BUFFER[0][1] = KEYS[KEY_A]
 
 
-charX = 0
-charY = 0
-timeout = 0
-
-
 def move_character():
     """
-    Move character on the game board.
+    Move a character on the game board.
     """
 
-    global charX, charY, timeout
+    global CHAR_X, CHAR_Y, TIMEOUT
 
     clear_buffer()
 
-    if timeout >= 100:
+    if TIMEOUT >= 100:
         if KEYS[KEY_W]:
-            charY += 1
-            timeout = 0
+            CHAR_Y += 1
+            TIMEOUT = 0
         if KEYS[KEY_A]:
-            charX -= 1
-            timeout = 0
+            CHAR_X -= 1
+            TIMEOUT = 0
         if KEYS[KEY_S]:
-            charY -= 1
-            timeout = 0
+            CHAR_Y -= 1
+            TIMEOUT = 0
         if KEYS[KEY_D]:
-            charX += 1
-            timeout = 0
+            CHAR_X += 1
+            TIMEOUT = 0
     else:
-        timeout += 1
+        TIMEOUT += 1
 
     # Check bounds on our new location.
-    if charX < 0:
-        charX = 0
-    if charX > 6:
-        charX = 6
-    if charY < 0:
-        charY = 0
-    if charY > 4:
-        charY = 4
+    if CHAR_X < 0:
+        CHAR_X = 0
+    if CHAR_X > 6:
+        CHAR_X = 6
+    if CHAR_Y < 0:
+        CHAR_Y = 0
+    if CHAR_Y > 4:
+        CHAR_Y = 4
 
     # Output character dot.
-    SCREEN_BUFFER[charX][charY] = 1
+    SCREEN_BUFFER[CHAR_X][CHAR_Y] = 1
 
 
 ################################
@@ -207,10 +250,4 @@ def your_program():
 
 
 if __name__ == "__main__":
-    while True:
-        key_test()
-        # move_character()
-        # your_program()
-
-        # Needed for simulation to run.
-        update_simulator()
+    main()
