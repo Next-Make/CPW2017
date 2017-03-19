52e7552fe2ad7d2acce023f80886de479163fcfd
gfyoung <gfyoung17@gmail.com>
2017-03-18 21:52:29 -0400

    Make filename of C code variable

--- cpw2017/games/upload.sh
+++ cpw2017/games/upload.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+name=$1
+
+if [ -z "$name" ]
+then
+    echo "Missing name of file"
+    exit 1
+fi
+
 # Compile the code
-avr-gcc -std=c99 -g -DF_CPU=8000000 -Wall -O1 -Werror -Wextra -mmcu=attiny88 -Wa,-ahlmns=Next_Make_CPW_2017.lst -c -o Next_Make_CPW_2017.o Next_Make_CPW_2017.c
-avr-gcc -std=c99 -g -DF_CPU=8000000 -Wall -O1 -Werror -Wextra -mmcu=attiny88 -o Next_Make_CPW_2017.elf Next_Make_CPW_2017.o
-avr-objcopy -j .text -j .data -O ihex Next_Make_CPW_2017.elf Next_Make_CPW_2017.hex
+avr-gcc -std=c99 -g -DF_CPU=8000000 -Wall -O1 -Werror -Wextra -mmcu=attiny88 -Wa,-ahlmns=$name.lst -c -o $name.o $name.c
+avr-gcc -std=c99 -g -DF_CPU=8000000 -Wall -O1 -Werror -Wextra -mmcu=attiny88 -o $name.elf $name.o
+avr-objcopy -j .text -j .data -O ihex $name.elf $name.hex
 
 # Program the compiled code onto board
-avrdude -p t88 -c avrisp2 -B 1 -v -U flash:w:Next_Make_CPW_2017.hex -U lfuse:w:0xEE:m
+avrdude -p t88 -c avrisp2 -B 1 -v -U flash:w:$name.hex -U lfuse:w:0xEE:m
