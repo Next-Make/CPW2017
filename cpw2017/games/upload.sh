99999b30702268df1fdd1108d6248714f0f78ecc
gfyoung <gfyoung17@gmail.com>
2017-03-18 23:24:24 -0400

    Fail upload if compilation fails

--- cpw2017/games/upload.sh
+++ cpw2017/games/upload.sh
@@ -9,9 +9,9 @@ then
 fi
 
 # Compile the code
-avr-gcc -std=c99 -g -DF_CPU=8000000 -Wall -O1 -Werror -Wextra -mmcu=attiny88 -Wa,-ahlmns=$name.lst -c -o $name.o $name.c
-avr-gcc -std=c99 -g -DF_CPU=8000000 -Wall -O1 -Werror -Wextra -mmcu=attiny88 -o $name.elf $name.o
-avr-objcopy -j .text -j .data -O ihex $name.elf $name.hex
+avr-gcc -std=c99 -g -DF_CPU=8000000 -Wall -O1 -Werror -Wextra -mmcu=attiny88 -Wa,-ahlmns=$name.lst -c -o $name.o $name.c || exit 1
+avr-gcc -std=c99 -g -DF_CPU=8000000 -Wall -O1 -Werror -Wextra -mmcu=attiny88 -o $name.elf $name.o || exit 1
+avr-objcopy -j .text -j .data -O ihex $name.elf $name.hex || exit 1
 
 # Program the compiled code onto board
 avrdude -p t88 -c avrisp2 -B 1 -v -U flash:w:$name.hex -U lfuse:w:0xEE:m
